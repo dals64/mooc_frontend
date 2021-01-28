e35076be557bdd0c36f18c5e0d4358808b7c6661
dals64 <lemotieuarold@gmail.com>
2021-01-28 11:54:31 +0100

    login, singup , accueil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { SignInServiceService } from './Services/sign-in-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'sti-frontend';
+
+  constructor(private service: SignInServiceService){}
+
+  public isLogged = false;
+
+  ngOnInit(){
+    this.isLogged = this.service.isLogged;
+  }
+
 }
