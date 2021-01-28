7f7405947a2f9e147af9d0996b8fc6d56b06e049
dals64 <lemotieuarold@gmail.com>
2021-01-28 17:36:38 +0100

    login & register ok

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -12,4 +12,61 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public isOk = true;
+
+  public error = null;
+
+  public
+
+  public formData = {
+    email: null,
+    password: null,
+    firstname: null,
+    lastname: null,
+    age: null,
+    sexe: null,
+    confirm: null
+  }
+
+  submit() {
+    if (this.validation()) {
+      console.log('ik')
+    }
+
+  }
+
+  validateMail(mail) {
+    let bool1 = false
+    let bool2 = false
+    let el1 = ['.']
+    let el2 = ['.']
+    for (let index = 0; index < mail.length; index++) {
+      if (el1.includes(mail[index])) {
+        bool1 = true
+      }
+    }
+    for (let index = 0; index < mail.length; index++) {
+      if (el2.includes(mail[index])) {
+        bool2 = true
+      }
+    }
+
+    return (bool1 && bool2)
+  }
+
+  validation() {
+    this.isOk = true;
+    if (this.validateMail(this.formData.email) === false) {
+      this.error = 'Entrer une adresse mail valide'
+      this.isOk = false
+      return !this.isOk
+    } else if ((this.formData.password === null) || (this.formData.password === '')) {
+      this.error = "Entrer votre mot de passe"
+      this.isOk = false
+      return !this.isOk
+    } else {
+      return this.isOk
+    }
+  }
+
 }
